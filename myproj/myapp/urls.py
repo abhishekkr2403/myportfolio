ae8117520b8a173a85395afb40810c3e98bf5e7f
AMANKANT312 <amankant312@users.noreply.github.com>
2023-10-19 13:12:26 +0530

    updated
    files are updated

--- myproj/myapp/urls.py
+++ myproj/myapp/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.Home,name='home'),
     path('home/<int:pk>',views.Catimg,name='home'),
+    path('hire/',views.hirepage,name='hire'),
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -3,7 +3,7 @@ from .models import Category,CatImage
 # Create your views here.
 def Home(request):
     allcategory=Category.objects.all()
-    return render(request,'myapp/home.html',{'category':allcategory})
+    return render(request,'myapp/home.html',{'category':allcategory,'home':'active'})
 def Catimg(request,pk):
     catdata=Category.objects.get(id=pk)
     print(catdata.title)
@@ -14,3 +14,5 @@ def Catimg(request,pk):
     #     print(data)
 
     return render(request,'myapp/catpage.html',{'data':data,'title':title})
+def hirepage(request):
+    return render(request,'myapp/hire.html',{'hire':'active'})
